6014e092da0a2dd4d021059a972c7e2035708917
pavel <nesoglasnyj.pavel@gmail.com>
2022-12-07 17:09:44 +0300

    верстка top header

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -6,33 +6,51 @@ import s from "./Profile.module.scss";
 function Profile() {
 	return (
 		<div className={s.container}>
-			<div className={s.profile}>
-				<div className={`${s.item} ${s.header}`}>
-					<div className={s.header__banner}>
-						<img className={s.banner} src="https://images.wallpaperscraft.ru/image/single/gorod_zdaniia_vektor_216924_1920x1080.jpg" alt="img" />
-					</div>
-					<nav className={s.header__nav}>
-						<ul className={s.navbar__link}>
-							<li className={s.link}>Friends</li>
-							<li className={s.link}>Dialogs</li>
-							<li className={s.link}>Photos</li>
-							<li className={s.link}>Videos</li>
+			<div className={s.top_header}>
+				<div className={s.top_header_banner}>
+					<img className={s.banner} src="https://html.crumina.net/html-olympus/img/top-header1.webp" alt="img" />
+				</div>
+				<div className={s.profile__section_item}>
+					<div className={s.section_item}>
+						<ul className={s.profile_menu}>
+							<li>Friends</li>
+							<li>Dialogs</li>
+							<li>Photos</li>
+							<li>Videos</li>
 						</ul>
-					</nav>
+					</div>
+					<div className={`${s.section_item} ${s.ml_auto}`}>
+						<ul className={s.profile_menu}>
+							<li>Friends</li>
+							<li>Dialogs</li>
+							<li>Photos</li>
+							<li>Videos</li>
+						</ul>
+					</div>
 				</div>
-				<div className={`${s.item} ${s.dialogs}`}>
-					<div className="">
-						<DialogItem name="Paul"/>
-						<DialogItem name="Mark"/>
-						<DialogItem name="Jane"/>
+				<div className={s.top_header_author}>
+					<a href="#" className={s.autor_img}>
+						<img src="https://html.crumina.net/html-olympus/img/author-main1.webp" alt="author" width='124px' height='124px'
+						/>
+					</a>
+					<div className={s.author_content}>
+						<a href="#" className={s.author_name}>James Spiegel</a>
+						<div className={s.country}>San Francisco, CA</div>
 					</div>
-					<div className="">
-						<Messages message="Hi, there!!!" />
-						<Messages message="Hi, Paul" />
-						<Messages message="what's up, guys?" />
-					</div>
+				</div>
+			</div>
+			<div className={`${s.item} ${s.dialogs}`}>
+				<div className="">
+					<DialogItem name="Paul" />
+					<DialogItem name="Mark" />
+					<DialogItem name="Jane" />
+				</div>
+				<div className="">
+					<Messages message="Hi, there!!!" />
+					<Messages message="Hi, Paul" />
+					<Messages message="what's up, guys?" />
+				</div>
 
-				</div>
 			</div>
 		</div>
 	);
