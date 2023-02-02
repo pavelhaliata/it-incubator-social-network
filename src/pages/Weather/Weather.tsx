8d1094f2e49809a48d6241e87c1f5e713ec5dac9
pavel <nesoglasnyj.pavel@gmail.com>
2023-02-02 17:03:43 +0300

    practice

--- src/pages/Weather/Weather.tsx
+++ src/pages/Weather/Weather.tsx
@@ -1,63 +1,5 @@
+import { type } from "@testing-library/user-event/dist/type"
 import { ChangeEvent, useEffect, useState } from "react"
-
-declare module namespace {
-
-	export interface Coord {
-		lon: number;
-		lat: number;
-	}
-
-	export interface Weather {
-		id: number;
-		main: string;
-		description: string;
-		icon: string;
-	}
-
-	export interface Main {
-		temp: number;
-		feels_like: number;
-		temp_min: number;
-		temp_max: number;
-		pressure: number;
-		humidity: number;
-	}
-
-	export interface Wind {
-		speed: number;
-		deg: number;
-	}
-
-	export interface Clouds {
-		all: number;
-	}
-
-	export interface Sys {
-		type: number;
-		id: number;
-		country: string;
-		sunrise: number;
-		sunset: number;
-	}
-
-	export interface RootObject {
-		coord: Coord;
-		weather: Weather[];
-		base: string;
-		main: Main;
-		visibility: number;
-		wind: Wind;
-		clouds: Clouds;
-		dt: number;
-		sys: Sys;
-		timezone: number;
-		id: number;
-		name: string;
-		cod: number;
-	}
-
-}
-
 
 
 type MainType = {
@@ -73,12 +15,15 @@ type WeatherPropsType = {
 	setStatePage: (value: string) => void
 }
 
+const API_KEY = 'bd4d8697c2213442afba131cd703e05a'
 
 export const Weather = ({ setStatePage }: WeatherPropsType) => {
-	const API_KEY = 'bd4d8697c2213442afba131cd703e05a'
+	
+	const date = new Date();
+	const today = date.toLocaleString('en-us', { day: 'numeric', weekday: 'long', month: 'long'})
 	
 	const [title, setTitle] = useState('')
-	const [city, setCity] = useState('minsk')
+	const [city, setCity] = useState('Minsk')
 	const [weatherInfo, setWeatherInfo] = useState<WeatherType>(
 		{
 			name: '',
@@ -109,13 +54,25 @@ export const Weather = ({ setStatePage }: WeatherPropsType) => {
 	return (
 
 		<>
-			<p>Город: {weatherInfo.name}</p>
-			<p>Температура: {Math.round(weatherInfo.main.temp)}&deg;</p>
-			<p>Ощущается как: {Math.round(weatherInfo.main.feels_like)}&deg;</p>
-
-			<input type="text" onChange={onChangeVaueHandler} placeholder="enter city" />
+			<input type="text" value={title} onChange={onChangeVaueHandler} placeholder="find city" />
 			<button onClick={onClickHandler}>enter</button>
-
+			<div className="weather">
+				<div className="weather__now">Temp: {Math.round(weatherInfo.main.temp)}&deg;</div>
+				<div className="weather__now__description">Feels like: {Math.round(weatherInfo.main.feels_like)}&deg;</div>
+				<ul className="weekly-forecast">
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+				</ul>
+				<div className="date_and_place">
+					<div className="date">{today}th</div>
+					<div className="place">{weatherInfo.name}</div>
+				</div>
+			</div>
 		</>
 	)
 } 
