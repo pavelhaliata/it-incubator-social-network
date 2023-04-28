d087977a2dc174c268321d14fb27cbe6cd6d6be5
pavel <nesoglasnyj.pavel@gmail.com>
2023-04-28 16:53:06 +0300

    Добалена строка поиска в компоненту header Добалена иконка в Sidebar

---
+++ src/components/Input/input.tsx
@@ -0,0 +1,37 @@
+import { ChangeEvent, KeyboardEvent, useState } from "react";
+
+type InputPropsType = {
+  placeholder?: string;
+  inputValue?: string;
+  className?: string
+};
+
+export const Input = ({ placeholder, inputValue, className }: InputPropsType) => {
+  const [value, setValue] = useState("");
+  const onKeyDownHandler = (event: KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter" && value) {
+      setValue("");
+      console.log("work");
+    }
+  };
+  const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    setValue(event.currentTarget.value);
+  };
+
+  return (
+    <form
+	className={className}
+      onSubmit={(e) => {
+        e.preventDefault();
+      }}
+    >
+      <input
+        value={value}
+        type="text"
+        placeholder={placeholder}
+        onKeyDown={onKeyDownHandler}
+        onChange={onChangeHandler}
+      />
+    </form>
+  );
+};
