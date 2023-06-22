ebb87f0a4486974c5ade9b5a47d87bf291020635
Pavel-Vladimirovich <nesoglasnyj.pavel@gmail.com>
2023-06-22 20:30:28 +0300

    обновлены пути для Blogpage.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,8 @@ import { Newspage } from './pages/Newspage/Newspage';
 import {DialogsContainer} from "./pages/Profilepage/Dialogs/DialogsContainer";
 import {FriendsContainer} from "./pages/Profilepage/Friends/FriendsContainer";
 import { ProfilePageContainer } from './pages/Profilepage/ProfilePageContainer';
-import {BlogPageContainer} from "./pages/BlogPage/BlogPageContainer";
+import {BlogPageContainer} from "./pages/Blogpage/BlogPageContainer";
+
 
 function App() {
 
@@ -15,14 +16,14 @@ function App() {
 	return (
 		<>
 			<Routes>
-				<Route path='/*' element={<Layout statePage={statePage} />}>
-					<Route path='/*' element={<ProfilePageContainer />}>
-						<Route path='dialogs' element={<DialogsContainer />} />
-						<Route path='friends' element={<FriendsContainer />} />
+				<Route path='/*' element={<Layout statePage={statePage}/>}>
+					<Route path='/*' element={<ProfilePageContainer/>}>
+						<Route path='dialogs' element={<DialogsContainer/>}/>
+						<Route path='friends' element={<FriendsContainer/>}/>
 					</Route>
-					<Route path='blog-page' element={<BlogPageContainer />}/>
-					<Route path='weather' element={<WeatherPage setStatePage={setStatePage} />} />
-					<Route path='news' element={<Newspage setStatePage={setStatePage} />} />
+					<Route path='blog-page' element={<BlogPageContainer/>}/>
+					<Route path='weather' element={<WeatherPage setStatePage={setStatePage}/>}/>
+					<Route path='news' element={<Newspage setStatePage={setStatePage}/>}/>
 				</Route>
 			</Routes>
 		</>
